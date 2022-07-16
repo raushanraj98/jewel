c15160abf56281875c414521ba3c2505aada304a
Raushan raj <65020988+raushanraj98@users.noreply.github.com>
2022-07-16 09:35:41 +0530

    test cases for  models

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,57 @@
 require "test_helper"
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @item = Item.new(name: "test", description: "test", price: "24", category_id: "2")
+  end
+
+  test "name should be present" do
+    @item.name = ""
+    assert_not @item.valid?
+  end
+
+  test "description should be present" do
+    @item.description = ""
+    assert_not @item.valid?
+  end
+
+  test "price should be present" do
+    @item.price = ""
+    assert_not @item.valid?
+  end
+
+  test "category_id should be present" do
+    @item.category_id = ""
+    assert_not @item.valid?
+  end
+
+  test "name should be less than 101 characters" do
+    @item.name = "a" * 101
+    assert_not @item.valid?
+  end
+
+  test "description should be less than 1001 characters" do
+    @item.description = "a" * 1001
+    assert_not @item.valid?
+  end
+
+  test "price should be greater than 0" do
+    @item.price = "0"
+    assert_not @item.valid?
+  end
+
+  test "category_id should be greater than 0" do
+    @item.category_id = "0"
+    assert_not @item.valid?
+  end
+
+  test "name must be greater than 3 character" do
+    @item.name = "a" * 2
+    assert_not @item.valid?
+  end
+
+  test "description must be greater than 10 character" do
+    @item.description = "a" * 9
+    assert_not @item.valid?
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(email: "", password: "")
+  end
+
+  test "email should be present" do
+    assert_not @user.valid?
+  end
+
+  test "password should be present" do
+    assert_not @user.valid?
+  end
+
+  test "email should be unique" do
+    @user.email = "xyz@xyz.com"
+    @user.password = "123456"
+    @user.save
+    @user2 = User.new(email: "xyz@xyz.com", password: "12345")
+    assert_not @user2.valid?
+  end
+
+  test "email should be less than 105 characters" do
+    @user.email = "a" * 105 + "@xyz.com"
+    @user.password = "123456"
+    assert_not @user.valid?
+  end
 end
